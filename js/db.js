bde52124c6103a07e99c713668c969981d451755
ags2121 <alexsilva88@gmail.com>
2017-08-06 19:10:34 -0400

    add carousel

--- js/db.js
+++ js/db.js
@@ -1,62 +1,65 @@
 let db = [
 	{
 		'name': 'narrative', 
-		'type': 'one_column_tiered_text_media', 
+		'type': 'one_column_tiered_text_media',
+		'media_type': "video",
 		'data': [{
 			'name': 'foo',
 			'thumbnail': 'foo_thumb.png',
 			'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-			'media': {'url': 'https://www.youtube.com/watch?v=WuX5p7OPpPU', 'type': 'video_embed'}
+			'media': {'url': 'https://www.youtube.com/embed/V5hOm8_3mJA?ecver=2', 'type': 'video_embed'}
 		},
 		{
 			'name': 'foo',
 			'thumbnail': 'foo_thumb.png',
 			'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-			'media': {'url': 'https://www.youtube.com/watch?v=WuX5p7OPpPU', 'type': 'video_embed'}
+			'media': {'url': 'https://www.youtube.com/embed/V5hOm8_3mJA?ecver=2', 'type': 'video_embed'}
 		},
 		{
 			'name': 'foo',
 			'thumbnail': 'foo_thumb.png',
 			'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-			'media': {'url': 'https://www.youtube.com/watch?v=WuX5p7OPpPU', 'type': 'video_embed'}
+			'media': {'url': 'https://www.youtube.com/embed/V5hOm8_3mJA?ecver=2', 'type': 'video_embed'}
 		}]
 	},
 	{
 		'name': 'docs', 
 		'type': 'one_column_tiered_text_media',
+		'media_type': "video",
 		'data': [{
 			'name': 'foo',
 			'thumbnail': 'roo_thumb.jpg',
 			'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-			'media': {'url': 'https://www.youtube.com/watch?v=WuX5p7OPpPU', 'type': 'video_embed'}
+			'media': {'url': 'https://www.youtube.com/embed/V5hOm8_3mJA?ecver=2', 'type': 'video_embed'}
 		},
 		{
 			'name': 'foo',
 			'thumbnail': 'roo_thumb.jpg',
 			'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-			'media': {'url': 'https://www.youtube.com/watch?v=WuX5p7OPpPU', 'type': 'video_embed'}
+			'media': {'url': 'https://www.youtube.com/embed/V5hOm8_3mJA?ecver=2', 'type': 'video_embed'}
 		},
 		{
 			'name': 'foo',
 			'thumbnail': 'roo_thumb.jpg',
 			'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-			'media': {'url': 'https://www.youtube.com/watch?v=WuX5p7OPpPU', 'type': 'video_embed'}
+			'media': {'url': 'https://www.youtube.com/embed/V5hOm8_3mJA?ecver=2', 'type': 'video_embed'}
 		}]
 	},
 	{
 		'name': 'stills', 
 		'type': 'two_column_text_media',
+		'media_type': "photo",
 		'data': [{
 			'name': 'foo',
 			'thumbnail': 'foo_thumb.png',
 			'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-			'media': {'url': 'foo_thumb.png', 'type': 'image_local'}
+			'media': {'url': 'assets/foo_thumb.png', 'type': 'image_local'}
 		},
 		{
 			'name': 'foo',
 			'thumbnail': 'foo_thumb.png',
 			'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-			'media': {'url': 'foo_thumb.png', 'type': 'image_local'}
+			'media': {'url': 'assets/foo_thumb.png', 'type': 'image_local'}
 		}]
 	},
 	{
@@ -66,7 +69,7 @@ let db = [
 			'name': 'foo',
 			'thumbnail': 'roo_thumb.jpg',
 			'description': 'I am J.A.G.',
-			'media': {'url': 'roo_thumb.png', 'type': 'image_local'}
+			'media': {'url': 'assets/roo_thumb.png', 'type': 'image_local'}
 		}]
 	},
 	{'name': 'contact', 'type': 'text', 'data': ['JAG@JAG.com']}
--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
 
 // Views
-let popupView = `<div class="popup hide"> <img src='assets/foo_thumb.png' /> </div>`;
 
-let createOneColumnTieredTextMediaView = (medias) => {
+let videoView = `
+	<div class="video-container">
+		<iframe width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>
+	</div>
+`;
+
+let photoView = `
+	<div class="photo-container">
+		<img src=""/>
+	</div>
+`;
+
+let createPopUpView = (mediaType) => {
+	if (!mediaType) {
+		return '';
+	}
+
+	let innerContainer = mediaType == "video" ? videoView : photoView;
+	return `
+		<div class="popup hide"> 
+			<div class="close"><a>&#10005;</a></div>
+			<div class="outer-container">
+				${innerContainer}
+			</div>
+		</div>
+	`;
+};
+
+let createOneColumnTieredTextMediaView = (media) => {
+	let medias = media['data'];
 	let i = 0;
+	let mediaType = media['media_type'];
+
 	return medias.map(media => {
 		let description = media['description'];
 		let thumbnail = media['thumbnail'];
 		let thumbnailHtml =
 			`
-			<li class="one-half column" data-media="">
-				<img class="thumbnail" src="assets/${thumbnail}"/>
+			<li class="one-half column">
+				<a> <img class="${mediaType ? 'thumbnail' : ''}" src="assets/${thumbnail}" data-media="${media['media']['url']}" /></a>
 			</li>
 			`;
 		let descriptionHtml = 
@@ -21,10 +51,13 @@ let createOneColumnTieredTextMediaView = (medias) => {
 			`;
 		let mediaHtml = ++i % 2 ? thumbnailHtml + descriptionHtml : descriptionHtml + thumbnailHtml;						
 		return '<div class="row tiered"><ul class="media-list">' + mediaHtml + '</ul></div>';
-	}).join('') + popupView;
+	}).join('') + createPopUpView(mediaType);
 };
 
-let createTwoColumnTextMediaView = (medias) => {
+let createTwoColumnTextMediaView = (media) => {
+	let medias = media['data'];
+	let mediaType = media['media_type'];
+
 	return partition(medias.slice(), 2).map(mediasForRow => {
 		return mediasForRow.reduce((t, media) => {
 			let description = media['description'];
@@ -33,16 +66,17 @@ let createTwoColumnTextMediaView = (medias) => {
 			return t +
 				`	
 				<li class="one-half column" data-media="">
-					<img class="thumbnail" src="assets/${thumbnail}"/>
+					<a> <img class="${mediaType ? 'thumbnail' : ''}" src="assets/${thumbnail}" data-media="${media['media']['url']}" /></a>
 					<div class="description">${description}</div>
 				</li>
 				`;
 
 		}, '<div class="row "><ul class="media-list">') + '</ul></div>';
-	}).join('') + popupView;
+	}).join('') + createPopUpView(mediaType);
 };
 
-let createTextView = (texts) => {
+let createTextView = (media) => {
+	let texts = media['data'];
 	return texts.map(text => `<div class="row text"><div class="column">${text}</div></div>`).join('');
 };
 
@@ -51,6 +85,8 @@ let viewFunctionLookup = {
 	'two_column_text_media': createTwoColumnTextMediaView,
 	'text': createTextView
 }
+
+// Create Views
 
 let navBarInnerHtml = db.reduce((t,i) => { 
 	let name = i['name']; 
@@ -65,20 +101,10 @@ document.querySelector('.navbar-list').innerHTML = navBarInnerHtml;
 
 let containersHtml = db.reduce((t, page) => { 
 	let name = page['name'];
-	let body = viewFunctionLookup[page['type']](page['data']);
+	let body = viewFunctionLookup[page['type']](page);
 	return t + `<div class="container hide ${name}">${body}</div>`;
 }, "");
 document.querySelector('.containers').innerHTML = containersHtml;
-
-let containerIds = db.map(e => e['name']);
-
-document.querySelectorAll('.navbar-link').forEach(navbarLink => navbarLink.addEventListener('click', e => {
-	showContainer(containerIds, e.currentTarget.getAttribute('href').replace('#',''));
-}));
-
-let requestedUrl = window.location.href.split('/').slice(-1)[0].replace('#','');
-
-showContainer(containerIds, requestedUrl);
 
 function showContainer(containerIds, containerIdToShow) {
 	let didShowContainer = false;
@@ -97,11 +123,41 @@ function showContainer(containerIds, containerIdToShow) {
 	}
 }
 
+let containerIds = db.map(e => e['name']);
+let requestedUrl = window.location.href.split('/').slice(-1)[0].replace('#','');
+
+showContainer(containerIds, requestedUrl);
+
 // Event handlers
+
+document.querySelectorAll('.navbar-link').forEach(navbarLink => navbarLink.addEventListener('click', e => {
+	showContainer(containerIds, e.currentTarget.getAttribute('href').replace('#',''));
+}));
+
+function setPopupMedia(popup, url) {
+  var matchingElements = [];
+  var allElements = popup.getElementsByTagName('*');
+  for (var i = 0, n = allElements.length; i < n; i++) {
+    if (allElements[i].getAttribute('src') !== null) {
+      // Element exists with attribute. Add to array.
+      matchingElements.push(allElements[i]);
+    }
+  }
+  matchingElements[0].setAttribute('src', url);
+}
+
 document.querySelectorAll('.thumbnail').forEach(t => {
 	t.addEventListener('click', e => {
 		let popup = e.currentTarget.closest('.container').querySelector('.popup');
+		let url = e.currentTarget.getAttribute('data-media');
+		setPopupMedia(popup, url);
 		popup.classList.remove('hide');
+	});
+});
+
+document.querySelectorAll('.close').forEach(close => {
+	close.addEventListener('click', e => {
+		e.currentTarget.closest('.popup').classList.add('hide');
 	});
 });
 
