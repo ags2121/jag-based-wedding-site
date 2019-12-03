9ed0cb5fe99bdf7e23ebad089dda2fa608198cdd
ags2121 <alexsilva88@gmail.com>
2019-12-02 21:18:08 -0500

    add stuff

--- js/db.js
+++ js/db.js
@@ -14,21 +14,65 @@ let db = [
 	{
 		'name': 'schedule',
 		'type': 'text',
-		'data': ["coming soon..."]
+		'data': [
+			'Friday June 19, 2020',
+			'5pm – 7pm',
+			'Happy hour welcome reception in Portland',
+			'Location TBD',
+			'<br/>',
+			'○○○',
+			'<br/>',
+			'Saturday June 20, 2020',
+			'3pm – 11pm',
+			'Ceremony, dinner, and dancing',
+			'Trefethen-Evergreen Improvement Association on Peaks Island.',
+			'<br/>',
+			'○○○',
+			'<br/>',
+			'<em>More details to come!</em>',
+			'<br/>',
+			'<img src="assets/teia.jpg" />'
+		]
 	},
 	{
 		'name': 'travel',
 		'type': 'text',
-		'data': ["coming soon..."]
-	},
-	{
-		'name': 'registry',
-		'type': 'text',
-		'data': ["coming soon..."]
+		'data': [
+			'<u>Getting There</u>',
+			'The local airport is in Portland, Maine (PWM). ',
+			'If you fly to Boston, there’s a direct bus to Portland from Logan Airport.',
+			'Casco Bay Ferry Lines will take guests to and from Peaks Island on a short, 15 minute ride. ',
+			'Ferries run every hour and a charter bus will take guests from the ferry to the venue.',
+			'<br/>',
+			'More details about day of travel to come!',
+			'<br/>',
+			'○○○',
+			'<br/>',
+			'<u>Accommodations</u>',
+			'Portland is a short ferry ride form Peaks Island and there are many places to stay there.',
+			'Here are a few recommendations:',
+			'<br/>',
+			'<em>Closest to Ferry Terminal in Portland</em>',
+			'Hilton Garden Inn – Downtown Portland',
+			'Hampton Inn Portland Downtown – Waterfront',
+			'AC Hotel by Marriott',
+			'<br/>',
+			'<em>Other Options</em>',
+			'Westin Portland Harborview Press Hotel',
+			'Portland Regency Hotel ',
+			'Danforth Inn',
+			'<br/>',
+			'<em>Staying on Peaks Island</em>',
+			'If you plan to stay on the island, we recommend looking at <a class="link" href="https://www.airbnb.com/s/Peaks-Island--ME">Airbnb</a> or <a class="link" href="https://www.vrbo.com/vacation-rentals/usa/maine/portland-casco-bay/peaks-island?k_clickid=Cj0KCQiAiZPvBRDZARIsAORkq7dbcOnY_C8vH1MYC9bBfx4zccxXQQy08iFHiNd-sWUOktChHQHjHCQaAh4sEALw_wcB&ds_cid=71700000026971550&ds_kids=p26947601768&ds_kid=43700026947601768&ksprof_id=700000001592040&ksdevice=c&ktarget=aud-410759985734:kwd-372504813761&kloct=&klocf=9004354&ds_aid=58700003367894209&ha_t=1t2:g:&gclid=Cj0KCQiAiZPvBRDZARIsAORkq7dbcOnY_C8vH1MYC9bBfx4zccxXQQy08iFHiNd-sWUOktChHQHjHCQaAh4sEALw_wcB&gclsrc=aw.ds">VRBO</a>. Many are week-long rentals and nightly rentals are less common, so it is good to search and book early. The island is generally walkable, but renting a golf cart or bikes is a nice way to get around. Keep in mind the venue is on the north side of the island (at Trefethen Ave and Island Ave), so a spot in the village center or closer the venue would be ideal.  Some ferries also allow cars if you would like to bring a car.',
+			'<br/>',
+			'<img src="assets/cant_wait.jpg" />'
+		]
 	},
 	{
 		'name': 'rsvp',
-		'type': 'text', 
-		'data': ["coming soon..."]
+		'type': 'text',
+		'data': [
+			'<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScOGaeLQDYoI8RNUQe5f1TXCi0U4g8pcf7G_MShiA-7YiGTDw/viewform?embedded=true" width="640" height="843" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>'
+		]
 	}
 ];
